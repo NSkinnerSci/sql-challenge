17b5be43f041e4af7176b4d8a5962f02947b9692
Nathan Skinner <nathanskinner@Nathans-MBP.attlocal.net>
2022-10-02 12:01:18 -0500

    Uploading analysis sql

--- data_schema.sql
+++ data_schema.sql
@@ -21,9 +21,10 @@ CREATE TABLE "departments" (
 );
 
 CREATE TABLE "dept_emp" (
-    "emp_no" INT   NOT NULL PRIMARY KEY,
+    "emp_no" INT   NOT NULL,
     "dept_no" VARCHAR   NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE "dept_manager" (
